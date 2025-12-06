e7f0e123653f4b524c033b80def6eb94a5d7d85c
Belisarius-159 <yusuforal7247@gmail.com>
2025-12-06 20:42:43 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,14 @@
+//Hello World! My name is Yusuf Oral and this is my "C" assignment for YMT113 class
+#include <stdio.h>
+
+int main()
+{
+    
+    for(int i = 1; i <= 10; i++)
+    {
+        int n = (i * 5);
+        int k = 100 - ((i - 1) * 10 + 1);
+        printf("%d\t%d\t%d\t\n", n, k, i);
+    }
+}
 
