7a8b5ec0ccc7756e19a8265963f414aa392d5527
Belisarius-159 <yusuforal7247@gmail.com>
2025-12-07 00:32:09 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,20 @@
+//Hello World! My name is Yusuf Oral and this is my "C" exercise for YMT113 class
+#include <stdio.h>
 
+int main()
+{
+    const int SIZE = 10;
+    int n, k, l;
+    for (n = 1; n <= SIZE; n++)
+    {
+        for (k = 1; k <= SIZE; k++)
+        {
+            l = n * k;
+            printf("%4d", l);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+    
+}
