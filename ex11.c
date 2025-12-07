bb5af5ce0324abad4f32c534e0352e00abd50ef1
Belisarius-159 <yusuforal7247@gmail.com>
2025-12-07 15:17:36 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,3 +1,4 @@
+//HELLO WORLD! My name is Yusuf Oral and this is my "C" exercise for YMT113 class.
 #include <stdio.h>
 int main()
 {
@@ -9,3 +10,4 @@ int main()
     printf(" *   * \n");
     printf("*     *\n");
 }
+//I am so sorry sir i know this is wrong however  couldn't figure out how to pull this off 
